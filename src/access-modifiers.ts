461a93a6ac5d0726125abfd1d390b8a9d7a3f427
sato9985 <hykt0603rsg@ezweb.ne.jp>
2024-04-19 15:43:25 +0900

    practice

--- src/access-modifiers.ts
+++ src/access-modifiers.ts
@@ -1,14 +1,16 @@
 export{};
 
-// クラス定義
+// 親クラス定義
 class Person{
   name:string;
-  age: number;
+  protected age: number;
+  protected nationality: string;
 
   // コンストラクタ
-  constructor(name:string,age:number){
+  constructor(name:string,age:number,nationality:string){
     this.name=name;
-    this.age=age
+    this.age=age;
+    this.nationality=nationality;
   }
 
   personInfo():string{
@@ -17,6 +19,22 @@ class Person{
     return `name:${this.name},age${this.age}`
   }
 }
-let tarou = new Person('太郎',31);
 
+class clone extends Person{
+  constructor(name:string,age:number,nationality:string){
+    super(name,age,nationality);
+  }
+
+  personInfo():string{
+    // return 'name: ${this.name},age:${this.age}';
+    // 上記のようにシングルクォーテーションではないので注意
+    return `name:${this.name},age${this.age},nationality:${this.nationality}`
+  }
+}
+let tarou = new Person('太郎',31,'日本');
+let tarouClone = new clone('太郎クローン',31,'日本');
+
+// console.log(tarou.name);
+// console.log(tarou.age);
 console.log(tarou.personInfo());
+console.log(tarouClone.personInfo());
